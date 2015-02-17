29de587c6faeed73be1a0c623e9f277109078b94
Valery Kuloyants <valery.kuloyants@live.de>
2015-02-17 16:03:40 +0100

    utf8 encode for mysql

--- service/Db.php
+++ service/Db.php
@@ -10,10 +10,11 @@ class Db
     {
         $config = getConfig()['db'];
         if (empty($instance)) {
-            self::$instance = @new mysqli($config['host'], $config['user'], $config['password'], $config['database']);
+            self::$instance = new mysqli($config['host'], $config['user'], $config['password'], $config['database']);
             if (mysqli_connect_errno()) {
                 throw new \Exception("Konnte keine Verbindung zu Datenbank aufbauen, MySQL meldete: {mysqli_connect_error()}");
             }
+            self::$instance->set_charset("utf8");
         }
         return self::$instance;
     }
