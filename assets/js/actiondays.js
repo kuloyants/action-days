730c712349b0b77e379d7a9c6a90f7a70fc83597
Valery Kuloyants <valery.kuloyants@live.de>
2015-03-08 21:07:36 +0100

    fixed double url-get parameter lang

--- assets/js/actiondays.js
+++ assets/js/actiondays.js
@@ -2,136 +2,136 @@
  * Created by Valery on 18.01.2015.
  */
 $(function() {
-	var $filters = $(".slider-buttons #filters");
-	$filters.on("click", "li", function(){
-		var $filter = $(this).find("span");
-		if (!$filter.hasClass('active')) {
-			$filters.find("span.active").removeClass('active');
-			$filter.addClass(" active");
-			$("#sliderlist div.active").removeClass('active').fadeOut('slow', 'linear', function(){
-				($("." + $filter.data('filter'))).fadeIn('slow', 'linear').addClass('active')
-			});
-		}
-	});
+    var $filters = $(".slider-buttons #filters");
+    $filters.on("click", "li", function(){
+        var $filter = $(this).find("span");
+        if (!$filter.hasClass('active')) {
+            $filters.find("span.active").removeClass('active');
+            $filter.addClass(" active");
+            $("#sliderlist div.active").removeClass('active').fadeOut('slow', 'linear', function(){
+                ($("." + $filter.data('filter'))).fadeIn('slow', 'linear').addClass('active')
+            });
+        }
+    });
 });
 
 $(document).ready(function () {
 
-	$("[data-role=languages]").on("click", "a", function(event) {
-		event.preventDefault();
-		var lang = $(this).data("url");
-		var newLocation = window.location.href.indexOf('?') != -1 ? "&lang=" + lang : "?lang=" + lang;
-		window.location = window.location + newLocation;
-	});
+    $("[data-role=languages]").on("click", "a", function(event) {
+        event.preventDefault();
+        var lang = $(this).data("url");
+        window.location = (window.location.href.replace(/[&\?]lang=[^&]*/i, ''))
+                            + (window.location.href.indexOf('?') != -1 ? "&lang=" + lang : "?lang=" + lang);
+    });
 
-	$(".match_score").each(function() {
-		if ($(this).text() == 2) {
-			var dataScore = $(this).data("score");
-			$(this).addClass("winner");
-			$(this).closest("tr").find("[data-player=" + dataScore + "]").addClass("winner");
-		}
-	});
+    $(".match_score").each(function() {
+        if ($(this).text() == 2) {
+            var dataScore = $(this).data("score");
+            $(this).addClass("winner");
+            $(this).closest("tr").find("[data-player=" + dataScore + "]").addClass("winner");
+        }
+    });
 
-	var $galleryContainer = $("#galleryContainer");
-	$galleryContainer.justifiedGallery({
-		rowHeight: 200,
-		fixedHeight: false,
-		lastRow: 'nojustify',
-		margins: 10,
-		randomize: false,
-		sizeRangeSuffixes: {
-			'lt100':'',
-			'lt240':'',
-			'lt320':'',
-			'lt500':'',
-			'lt640':'',
-			'lt1024':''
-		}
-	}).on('jg.complete', function () {
-		$galleryContainer.find('a').swipebox({
-			//useCSS : false // false will force the use of jQuery for animations
-		});
-	});
+    var $galleryContainer = $("#galleryContainer");
+    $galleryContainer.justifiedGallery({
+        rowHeight: 200,
+        fixedHeight: false,
+        lastRow: 'nojustify',
+        margins: 10,
+        randomize: false,
+        sizeRangeSuffixes: {
+            'lt100':'',
+            'lt240':'',
+            'lt320':'',
+            'lt500':'',
+            'lt640':'',
+            'lt1024':''
+        }
+    }).on('jg.complete', function () {
+        $galleryContainer.find('a').swipebox({
+            //useCSS : false // false will force the use of jQuery for animations
+        });
+    });
 
-	$(".moreDialog").on("click", function(event) {
-		event.preventDefault();
+    $(".moreDialog").on("click", function(event) {
+        event.preventDefault();
 
-		var playerId = $(this).data('profile');
-		$.ajax({
-			url: "?section=profile&profile=" + playerId,
-			type: "GET",
-			success: function(response) {
-				var title = $(response).find("h5.playerFullName").text();
+        var playerId = $(this).data('profile');
+        $.ajax({
+            url: "?section=profile&profile=" + playerId,
+            type: "GET",
+            success: function(response) {
+                var title = $(response).find("h5.playerFullName").text();
 
 
-				$(response).dialog({
-					create:function () {
-						$(this).closest(".ui-dialog")
-							.find(".ui-dialog-titlebar-close") // the first button
-							.hide();
-						$(this).closest(".ui-dialog")
-							.find(".ui-dialog-buttonset button") // the first button
-							.addClass("btn btn-blue");
-					},
-					dialogClass: "no-close",
-					show: 'fadeIn',
-					hide: 'fadeOut',
-					modal: true,
-					draggable: false,
-					resizable: false,
-					closeOnEscape: true,
-					autoOpen: true,
-					width: '500px',
-					title: title,
-					buttons: [
-						{
-							text: "OK",
-							click: function() {
-								$( this ).dialog( "close" );
-							}
-						}
-					]
-				})
-			}
-		})
-	});
+                $(response).dialog({
+                    create:function () {
+                        $(this).closest(".ui-dialog")
+                            .find(".ui-dialog-titlebar-close") // the first button
+                            .hide();
+                        $(this).closest(".ui-dialog")
+                            .find(".ui-dialog-buttonset button") // the first button
+                            .addClass("btn btn-blue");
+                    },
+                    dialogClass: "no-close",
+                    show: 'fadeIn',
+                    hide: 'fadeOut',
+                    modal: true,
+                    draggable: false,
+                    resizable: false,
+                    closeOnEscape: true,
+                    autoOpen: true,
+                    width: '500px',
+                    title: title,
+                    buttons: [
+                        {
+                            text: "OK",
+                            click: function() {
+                                $( this ).dialog( "close" );
+                            }
+                        }
+                    ]
+                })
+            }
+        })
+    });
 
-	$('.more').on('click', function(event) {
-		event.preventDefault();
+    $('.more').on('click', function(event) {
+        event.preventDefault();
 
-		var href = $(this).find('a').attr('href'),
-			portfolioList = $('#sliderlist'),
-			content = $('#loaded-content');
+        var href = $(this).find('a').attr('href'),
+            portfolioList = $('#sliderlist'),
+            content = $('#loaded-content');
 
-		portfolioList.animate({'marginLeft':'-120%'},{duration:400,queue:false});
-		portfolioList.fadeOut(400);
-		setTimeout(function(){
-			console.log($('#loader'));
-			$('#loader').show();
-			console.log($('#loader'));
-		},400);
-		setTimeout(function(){
-			content.load(href, function() {
-				$('#loaded-content meta').remove();
-				$('#loader').hide();
-				content.fadeIn(600);
-				$('#back-button').fadeIn(600);
-			});
-		},800);
+        portfolioList.animate({'marginLeft':'-120%'},{duration:400,queue:false});
+        portfolioList.fadeOut(400);
+        setTimeout(function(){
+            console.log($('#loader'));
+            $('#loader').show();
+            console.log($('#loader'));
+        },400);
+        setTimeout(function(){
+            content.load(href, function() {
+                $('#loaded-content meta').remove();
+                $('#loader').hide();
+                content.fadeIn(600);
+                $('#back-button').fadeIn(600);
+            });
+        },800);
 
-	});
+    });
 
-	$('#back-button').on('click', function(event) {
-		event.preventDefault();
+    $('#back-button').on('click', function(event) {
+        event.preventDefault();
 
-		var portfolioList = $('#sliderlist')
-		content = $('#loaded-content');
+        var portfolioList = $('#sliderlist')
+        content = $('#loaded-content');
 
-		content.fadeOut(400);
-		$('#back-button').fadeOut(400);
-		setTimeout(function(){
-			portfolioList.animate({'marginLeft':'0'},{duration:400,queue:false});
-			portfolioList.fadeIn(600);
-		},800);
-	});
+        content.fadeOut(400);
+        $('#back-button').fadeOut(400);
+        setTimeout(function(){
+            portfolioList.animate({'marginLeft':'0'},{duration:400,queue:false});
+            portfolioList.fadeIn(600);
+        },800);
+    });
 });
