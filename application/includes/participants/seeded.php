972feb9b42da613905cbb7b3287ec3ababefc903
Valery Kuloyants <valery.kuloyants@live.de>
2015-03-19 08:52:36 +0100

    seed england

--- application/includes/participants/seeded.php
+++ application/includes/participants/seeded.php
@@ -12,7 +12,8 @@ SELECT
     p.country_code,
     pr.reg_status
 FROM player AS p INNER JOIN player_registration AS pr ON p.id = pr.player_id
-WHERE pr.reg_status in ('seeded')";
+WHERE pr.reg_status in ('seeded')
+ORDER BY p.updated DESC";
 $result = $db->query($sql);
 if (!$result) {
     throw new \Exception('Konnte den Folgenden Query nicht senden: '.$sql."<br />\nFehlermeldung: ".$db->error);
